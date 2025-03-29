54bbab122afab6aa044aa2d5c340ab184ecfb7ff
n1p <107926733+twlnky@users.noreply.github.com>
2025-03-29 20:33:10 +0300

    Brand new start - clean history

---
+++ src/main/java/rut/miit/tech/springasync/SpringAsyncApplication.java
@@ -0,0 +1,13 @@
+package rut.miit.tech.springasync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAsyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAsyncApplication.class, args);
+    }
+    public static void pull(){return;}
+}
